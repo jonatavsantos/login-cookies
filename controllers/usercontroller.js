6b82bc6833e1f04657605e961f49ca0ad40d8d26
jonatavsantos <surinegann@gmail.com>
2023-12-31 02:02:02 -0300

    Bcrypt hash

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,9 +1,16 @@
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+
+const saltRounds = Number(process.env.SALT_ROUNDS)
 
 async function createUser(req, res) {
 
     try {
         const data = req.body
+
+        const hash = await bcrypt.hash(data.password, saltRounds)
+
+        data.password = hash
 
         const newuser = await User.create(data)
 
